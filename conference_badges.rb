5656452bb5e503c816d3eb77b33760ba2877bd76
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-06 14:49:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def batch_badge_creator(attendees)
     "Hello, my name is #{attendee}."
 end
 
-def assign_rooms(rooms)
+def assign_rooms(attendees)
   attendees.each_with_index.map do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+=}"
 end
@@ -20,4 +20,9 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
+end
+
+assign_rooms(attendees).each do |assignment|
+  puts assignment
+end
 end
