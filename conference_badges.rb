1024ff8bfefed5200f610401f0f431887c7954f4
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-06 14:38:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,15 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
+
+def batch_badge_creator(array)
+  array = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+end
+
+def assign_rooms(rooms)
+end
+
+def printer(results)
+  batch_badge_creator=[]
+  
+end
